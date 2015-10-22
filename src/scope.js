cb9360f71dd7870c10e1bc9ba421749881f757ac
livoras <livoras@163.com>
2015-10-22 23:49:38 +0800

    add scopes

--- src/scope.js
+++ src/scope.js
@@ -1,9 +1,30 @@
-function Scope() {
+import $ from "cheerio"
 
+class Scope {
+  constructor(key) {
+    this.key = key
+    this.$ = $.load("<div class='scope'></div>")
+    this.$el = this.$("div.scope").eq(0)
+    this.makeHeading()
+  }
+  makeHeading() {
+    this.$el.prepend(`<div class="heading">${this.key}</div>`)
+  }
+  toStr() {
+    return this.$.html()
+  }
+  append(scope) {
+    this.$el.append(scope.$el)
+  }
 }
 
-function ArrayScope() {
-
+class ArrayScope extends Scope {
+  constructor(key) {
+    super(key)
+    this.$ = $.load("<div class='array-scope'></div>")
+    this.$el = this.$("div.array-scope").eq(0)
+    this.makeHeading()
+  }
 }
 
 export {Scope, ArrayScope}
--- test/scope-spec.js
+++ test/scope-spec.js
@@ -1,12 +1,21 @@
+import {ArrayScope, Scope} from "../src/scope"
 import chai from "chai"
 
 chai.should()
 
 describe("Test for fun", function() {
-  it("should have value a", function() {
-    "a".should.be.equal("a")
+  it("Scope should serialize html", function() {
+    var scope = new Scope("fuk")
+    scope.toStr().should.be.equal('<div class="scope"><div class="heading">fuk</div></div>')
   })
-  it("should have value b", function() {
-    "b".should.be.equal("b")
+  it("Test scope append", function() {
+    var scope = new Scope("fuk")
+    var scope2 = new Scope("fuk2")
+    scope.append(scope2)
+    scope.toStr().should.be.equal('<div class="scope"><div class="heading">fuk</div><div class="scope"><div class="heading">fuk2</div></div></div>')
+  })
+  it("Test array scope", function() {
+    var arrayScope = new ArrayScope("array-fuk")
+    arrayScope.toStr().should.be.equal('<div class="array-scope"><div class="heading">array-fuk</div></div>')
   })
 })
